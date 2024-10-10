dc46405de4a5f4c2eca1350149af0e86d5493c07
Sohail Asif <sohailasuf123@gmail.com>
2024-10-11 03:23:04 +0500

    Your commit message

--- backend/db/dbConnection.js
+++ backend/db/dbConnection.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 const DBConnection= async ()=>{
-    const MONGODB_URI='mongodb+srv://sohailasuf123:Sohail@cluster0.joj5j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0+srv://SohailAsif1:SohailAsif1@cluster0.7mm9cgw.mongodb.net/'
+    const MONGODB_URI=process.env.MONGO_URI
     try {
         await mongoose.connect(MONGODB_URI,{ useNewUrlParser:true})
         console.log("db connect");
