f6bea1a18106148ca35b8868bc9b59d33c6babd8
Sohail Asif <sohailasuf123@gmail.com>
2024-10-11 03:50:22 +0500

    Your commit message

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',  // Ensure this points to the correct output directory
+  },
 });
